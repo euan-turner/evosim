0c5afc72dc3856e72b1fef899abaace08e8c985f
Euan Turner <euanoturner@gmail.com>
2024-08-25 16:01:38 +0100

    Seal objectives trait

--- src/main/evo/subjects/Objective.scala
+++ src/main/evo/subjects/Objective.scala
@@ -8,5 +8,26 @@ import breeze.linalg.DenseVector
   *   Food Source (Edible extends Objective)
   *   Edge of map (for end of generation)
   */
-trait Objective:
+sealed trait Objective:
   def getPosition: DenseVector[Double] 
+  def exists: Boolean
+
+/**
+* Describes anything that an Organism may eat
+* Provides energy to its consumer
+* For example:
+*   Food
+*   Prey Organism
+*/
+trait Edible extends Objective:
+  private var eaten = false
+
+  def getEnergy: Double
+  def setPosition(pos: DenseVector[Double]): Unit
+  def eat(): Unit = 
+    eaten = true
+  override def exists: Boolean = !eaten
+
+class Position(val pos: DenseVector[Double]) extends Objective:
+  override def getPosition = pos
+  override def exists = true
